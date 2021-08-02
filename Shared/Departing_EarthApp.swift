c3513ab5460b9dc8c206a5403502de8df98ebeb0
Matthew Folbigg <matthew.folbigg@icloud.com>
2021-08-02 16:08:12 +0100

    Initial Commit

---
+++ Shared/Departing_EarthApp.swift
@@ -0,0 +1,20 @@
+//
+//  Departing_EarthApp.swift
+//  Shared
+//
+//  Created by Matthew Folbigg on 02/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct Departing_EarthApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
