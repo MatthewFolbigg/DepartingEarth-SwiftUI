4aca90366db00b2e7598a7d0e247b554f8fec1bf
Matthew Folbigg <matthew.folbigg@icloud.com>
2021-08-19 13:16:53 +0100

    Added new style status section to launch detail

--- Shared/Views/LaunchDetailView.swift
+++ Shared/Views/LaunchDetailView.swift
@@ -20,14 +20,16 @@ struct LaunchDetailView: View {
     
     var body: some View {
         ScrollView {
-            VStack(alignment: .leading, spacing: 20) {
+            VStack(alignment: .leading, spacing: 15) {
                 HStack {
                     Spacer()
                     countdown
                         .aspectRatio(CGSize(width: 11, height: 1), contentMode: .fit)
                         .font(.system(.body, design: .default))
+                        .scaleEffect(1.2)
                     Spacer()
                 }
+                statusSection
                 missionSection
                     .onTapGesture {
                         withAnimation(.easeOut) {
@@ -51,6 +53,28 @@ struct LaunchDetailView: View {
         )
     }
     
+    var statusSection: some View {
+        //TODO: If on hold add hold reason
+        //TODO: Window Start Window End timeline
+        VStack(alignment: .leading, spacing: 6) {
+            HStack {
+                Text("Status").font(.title2).fontWeight(.medium)
+                Spacer()
+            }
+            Label(
+                title: { Text(launch.status?.currentSituation.name ?? "") },
+                icon: { launch.status?.icon }
+            )
+            Label(
+                title: { Text(launch.dateString()) },
+                icon: { Image(systemName: "calendar") }
+            )
+            Text(launch.status?.infoText ?? "").font(.caption).fixedSize(horizontal: false, vertical: /*@START_MENU_TOKEN@*/true/*@END_MENU_TOKEN@*/)
+        }
+        .foregroundColor(.ui.greyBlueForeground)
+        .padding(20)
+        .background(Color.ui.greyBlueBackground.clipShape(RoundedRectangle(cornerRadius: 20)).opacity(0.5))
+    }
     
     var missionSection: some View {
         HStack {
@@ -68,21 +92,22 @@ struct LaunchDetailView: View {
                 icon: { Image(systemName: "person.2") }
             )
             Label(
-                title: { Text("\(launch.rocket?.name ?? "") - \(launch.rocket?.variant ?? "") ") },
+                title: { Text(launch.rocket?.name ?? "")},
                 icon: { Image(systemName: "airplane") }
             )
             Label(
                 title: { Text(launch.mission?.type ?? "") },
-                icon: { Image(systemName: "shippingbox") }
+                icon: { Image(systemName: launch.mission?.symbolForType ?? "") }
             )
             Label(
                 title: { Text(launch.mission?.orbit?.name ?? "") },
                 icon: { Image(systemName: "circle.dashed") }
             )
-            Spacer()
             if missionIsExpanded {
+                Spacer()
                 Text(launch.mission?.infoText ?? "").font(.system(.body)).fontWeight(.thin)
                     .foregroundColor(.primary)
+                    //TODO: improve transition. currently overlaps other text on rapid press
                     .transition(.asymmetric(
                         insertion: .move(edge: .top).combined(with: .opacity.animation(.easeInOut.delay(0.2))),
                         removal: .move(edge: .top).combined(with: .opacity.animation(.easeInOut(duration: 0.05)))
@@ -90,24 +115,12 @@ struct LaunchDetailView: View {
                 )
             }
         }
-            .foregroundColor(.ui.greyBlueForeground)
         }
+        .foregroundColor(.ui.greyBlueForeground)
         .padding(20)
         .background(Color.ui.greyBlueBackground.clipShape(RoundedRectangle(cornerRadius: 20)).opacity(0.5))
-        .animation(.easeOut)
     }
-    
-    
-        
-//    @State var imageFetchCancellable: AnyCancellable?
-//    LaunchLibraryApiClient.shared.fetchImageData(
-//        url: URL(string: launch?.provider?.logoUrl ?? ""),
-//        cancellable: $imageFetchCancellable) { image in
-//        withAnimation(.easeInOut(duration: 0.2)) {
-//            self.logoImage = image
-//        }
-//    }
-    
+
 }
 
 
